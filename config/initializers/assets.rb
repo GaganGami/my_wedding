d8fb4bc8094aa943dea3e8c809fdc623cced128c
Gagan Gami <gagangami@gmail.com>
2018-04-18 02:35:28 +0530

    implement lazyload

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( active_admin.js.coffee active_admin.css )
+Rails.application.config.assets.precompile += %w(jquery.lazyload.js main.js active_admin.js.coffee active_admin.css )
