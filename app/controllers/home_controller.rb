bd8c86c711eb882b1f35befaa72f765483c32a7f
Gagan Gami <gagangami@gmail.com>
2018-04-20 16:07:07 +0530

    set memory image dynamically

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
   	@groups = (1..42).to_a.shuffle.in_groups(4, false)
+  	@memories = Memory.order('random()').in_groups(4,false)
   end
 end
