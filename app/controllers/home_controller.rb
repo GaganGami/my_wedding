07019d4ad1e3ddd46e763009c9840bbbf00371b4
Gagan Gami <gagangami@gmail.com>
2018-04-20 16:23:50 +0530

    only urls of s3

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
   	@groups = (1..42).to_a.shuffle.in_groups(4, false)
-  	@memories = Memory.order('random()').in_groups(4,false)
+  	@memories = Memory.order('random()').map{|p| p.photo.url}.in_groups(4,false)
   end
 end
